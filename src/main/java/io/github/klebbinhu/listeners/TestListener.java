3189ee913eb2dbd7d8087278a36c216e9a084ef1
GalaxyCrush <jlatag400@gmail.com>
2023-04-23 02:31:50 +0100

    VC connected

--- src/main/java/io/github/klebbinhu/listeners/TestListener.java
+++ src/main/java/io/github/klebbinhu/listeners/TestListener.java
@@ -1,17 +1,25 @@
 package io.github.klebbinhu.listeners;
 
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 public class TestListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent e){
-        if(e.getAuthor().isBot()){
+        if(e.getAuthor().isBot() ||!e.isFromGuild()){
             return;
         }
-
+        if(e.getMessage().getContentRaw().startsWith("!play")){
+            Guild guild = e.getGuild();
+            VoiceChannel vc = guild.getVoiceChannelById("952948011548438598");
+            AudioManager am = guild.getAudioManager();
+            am.openAudioConnection(vc);
+        }
         Message message = e.getMessage();
         String content = message.getContentRaw();
         if (content.equals("!ping")){
