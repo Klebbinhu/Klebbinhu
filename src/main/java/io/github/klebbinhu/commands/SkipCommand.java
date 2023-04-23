a2719fe82f7a054de6e503c78e51452750733196
itsTaylz <itstaylz.main@gmail.com>
2023-04-23 06:16:48 +0100

    MUSIC

---
+++ src/main/java/io/github/klebbinhu/commands/SkipCommand.java
@@ -0,0 +1,4 @@
+package io.github.klebbinhu.commands;
+
+public class SkipCommand {
+}
--- src/main/java/io/github/klebbinhu/listeners/TestListener.java
+++ src/main/java/io/github/klebbinhu/listeners/TestListener.java
@@ -1,15 +1,9 @@
 package io.github.klebbinhu.listeners;
 
-import io.github.klebbinhu.music.MusicController;
 import net.dv8tion.jda.api.entities.*;
-import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
-import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-import net.dv8tion.jda.api.managers.AudioManager;
 
 public class TestListener extends ListenerAdapter {
     @Override
@@ -22,38 +16,6 @@ public class TestListener extends ListenerAdapter {
         if(content.equals("Feio")){
             MessageChannel channel = e.getChannel();
             channel.sendMessage("Tu es").queue();
-        }
-    }
-
-    @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        switch (event.getName()) {
-            case "play" -> {
-                event.deferReply().queue();
-                String link = event.getOption("link", OptionMapping::getAsString);
-                Member member = event.getMember();
-                if (member == null || event.getGuild() == null)
-                    return;
-                GuildVoiceState voiceState = member.getVoiceState();
-                if (voiceState == null || voiceState.getChannel() == null) {
-                    event.getHook().editOriginal("Error! You need to be in a voice channel to use this command.").queue();
-                    return;
-                }
-                VoiceChannel voiceChannel = voiceState.getChannel().asVoiceChannel();
-                MusicController.getInstance().queue(link, result -> {
-                    switch (result) {
-                        case FAILED -> event.getHook().editOriginal("Failed to load!").queue();
-                        case NO_MATCH -> event.getHook().editOriginal("No match!").queue();
-                        case SUCCESS -> {
-                            Guild guild = event.getGuild();
-                            AudioManager am = guild.getAudioManager();
-                            am.setSendingHandler(MusicController.getInstance().getAudioHandler());
-                            am.openAudioConnection(voiceChannel);
-                            event.getHook().editOriginal("Started playing!").queue();
-                        }
-                    }
-                });
-            }
         }
     }
 }
